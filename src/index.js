9cfafc97fc12e1278339263acda997a324cae075
Madi2807 <diapohribna@gmail.com>
2023-11-30 19:18:29 +0100

    add the form submit event

--- src/index.js
+++ src/index.js
@@ -1,52 +1,53 @@
 function getWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector("#icon");
-   
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-    cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML= formatDate(date);
-    descriptionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}$`;
-    windSpeedElement.innerHTML = `$(response.data.wind.speed)km/h`;
-    
-    console.log(response.data.temperature);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  console.log(response.data.temperature);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", search);
 
 function search(event) {
-event.preventDefault();
-let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#currently-city");
-let city = searchInputElement.value;
-cityElement.innerHTML = city;
- searchCity=(searchInput.value);
-}  
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  let city = searchInput.value;
+  cityElement.innerHTML = city;
+  searchCity(searchInput.value);
+}
 
 function searchCity(city) {
-let apiKey = "1of0t314d94be2d47af94e3feebe0e6d";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(getWeather);
+  let apiKey = "1of0t314d94be2d47af94e3feebe0e6d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getWeather);
 }
 function formatDate(date) {
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = [
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
-  ];   
+    "Saturday",
+  ];
   let day = date.getDay();
-    return `${hours}:${minutes}`;
-  }
-
+  return `${hours}:${minutes}`;
 }
