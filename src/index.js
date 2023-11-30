1ce1b26cb10a9cbe399c6709b4cd17c700704891
Madi2807 <diapohribna@gmail.com>
2023-11-30 12:13:56 +0100

    add some changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function getWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
@@ -8,16 +8,30 @@ function getWeather(response) {
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
-    
-    iconElement.innerHTML = `<img src="`${response.data.condition.icon_url}" class="weather-app-icon" />`;
+   
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML= formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}$`;
     windSpeedElement.innerHTML = `$(response.data.wind.speed)km/h`;
-    temperatureElement.innerHTML = Math.round(temperature);   
-    }
+    
 
+
+function search(event) {
+event.preventDefault();
+let searchInput = document.querySelector("#search-form-input");
+let cityElement = document.querySelector("#currently-city");
+let city = searchInputElement.value;
+cityElement.innerHTML = city;
+ searchCity=(searchInput.value);
+}  
+
+function searchCity(city) {
+let apiKey = "1of0t314d94be2d47af94e3feebe0e6d";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(getWeather);
+}
 function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
@@ -30,27 +44,8 @@ function formatDate(date) {
     "Friday",
     "Saturday"
   ];   
-  let day = days[date.getDay()];
-  if (minutes < 10) {
-  minutes =`0${minutes}`;
-  } 
-  return `${hours}:${minutes}`;
+  let day = date.getDay();
+    return `${hours}:${minutes}`;
+  }
+
 }
-
-function searchCity(city) {
-let apiKey = "1of0t314d94be2d47af94e3feebe0e6d";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(getWeather);
-}
-
-function handleSearchSubmit(event) {
-event.preventDefault();
-let searchInput = document.querySelector("#search-form-input");
-    
- searchCity(searchInput.value);
-}   
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Orlando");
